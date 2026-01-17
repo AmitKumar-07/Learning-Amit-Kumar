aed8b692d2d5dec600c0813aba485c1618f2a53f
Amit Kumar <justamitt@Amits-MacBook-Air.local>
2026-01-18 04:34:31 +0530

    changes

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,18 @@ import java.util.*;
 public class Main {
         public static void main(String[] args) throws Exception {
 
-
-            String test = "abs";
+            Map<Integer, String> mp = new HashMap<>();
+            mp.put(12, "abc");
+            mp.put(1, "bhu");
+            mp.put(2, "iit");
+            for(Map.Entry<Integer, String> ele : mp.entrySet()){
+                if(ele.getKey()==2){
+                    ele.setValue("nit");
+                }
+            }
+            for(Map.Entry<Integer, String> ele : mp.entrySet()){
+                System.out.println(ele.getValue());
+            }
 
         }
 
